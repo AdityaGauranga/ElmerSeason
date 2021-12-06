4669a14a34fd79d3cabe42d0a60f5e9686702ad5
AdityaGauranga <aaditya.sarma108@gmail.com>
2021-12-06 17:52:31 +0530

    translation and error functions added

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("txt-input");
 var outputdiv = document.querySelector("output");
+
+var serverURL = "https://api.funtranslations.com/translate/fudd.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("An error came up :/", error)
+    alert("It seems an unexpected error came up. Please try again after few moments later")
+}
