fda5e704717251153fcc6e7996f83886bf1cc1ac
AdityaGauranga <aaditya.sarma108@gmail.com>
2021-12-06 17:56:07 +0530

    fetch and clickhandler function

--- app.js
+++ app.js
@@ -12,3 +12,14 @@ function errorHandler(error) {
     console.log("An error came up :/", error)
     alert("It seems an unexpected error came up. Please try again after few moments later")
 }
+
+function clickHandler() {
+    var inputText = textInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedTxt = json.contents.translated;
+        outputdiv.innerText = translatedTxt;
+    })
+}
